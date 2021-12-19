9cd84ddbab8dc9394f8d3e2b409bf6308a9dbd11
Tyler Butler <tyler@tylerbutler.com>
2021-12-18 22:29:23 -0800

    Initialize

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO: download sources file from URL defined in env variable
+pybase16 update -cv
+
+pybase16 build -v
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pip3 install pybase16-builder
